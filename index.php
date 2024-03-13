fd184e725868991c6997d1bef90a65ef8ee9e8e7
Bryan Mello Barros <bryanmellobarros@gmail.com>
2024-03-13 15:15:39 -0300

    Tela de Login

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -7,6 +9,8 @@ use \Slim\Slim;
 use \Hcode\Page;
 
 use \Hcode\PageAdmin;
+
+use \Hcode\Model\User; 
 
 $app = new Slim();
 
@@ -21,10 +25,40 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin(["header"=>false, "footer"=>false]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+    
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+
+	exit;
 
 });
 
